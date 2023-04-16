b69a4105535adde1d5d3f7db0e83e30c476eee30
Administrator <Administrator@NGOXUANHINH2801>
2023-04-16 18:57:50 +0700

    Update contact form

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,93 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+/*
+THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
+*/
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'ngoxuanhinham123@gmail.com';
+require '../assets/vendor/php-email-form/PHPMailerAutoload.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+/*
+*  CONFIGURE EVERYTHING HERE
+*/
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+// an email address that will be in the From field of the email.
+$fromEmail = 'ngoxuanhinhad4@gmail.com';
+$fromName = 'Ad4';
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+// an email address that will receive the email with the output of the form
+$sendToEmail = 'ngoxuanhinham123@gmail.com';
+$sendToName = 'Am3';
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// subject of the email
+$subject = 'New message from contact form';
 
-  echo $contact->send();
-?>
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message');
+
+// message that will be displayed when everything is OK :)
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+
+// If something goes wrong, we will display this message.
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+/*
+*  LET'S DO THE SENDING
+*/
+
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
+
+try
+{
+    
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+    
+    $emailTextHtml = "<h1>You have a new message from your contact form</h1><hr>";
+    $emailTextHtml .= "<table>";
+    
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email
+        if (isset($fields[$key])) {
+            $emailTextHtml .= "<tr><th>$fields[$key]</th><td>$value</td></tr>";
+        }
+    }
+    $emailTextHtml .= "</table><hr>";
+    $emailTextHtml .= "<p>Have a nice day,<br>Best,<br>Ondrej</p>";
+    
+    $mail = new PHPMailer;
+    
+    $mail->setFrom($fromEmail, $fromName);
+    $mail->addAddress($sendToEmail, $sendToName); // you can add more addresses by simply adding another line with $mail->addAddress();
+    $mail->addReplyTo($from);
+    
+    $mail->isHTML(true);
+    
+    $mail->Subject = $subject;
+    $mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
+    
+    
+    if(!$mail->send()) {
+        throw new \Exception('I could not send the email.' . $mail->ErrorInfo);
+    }
+    
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    // $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+    $responseArray = array('type' => 'danger', 'message' => $e->getMessage());
+}
+
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+    
+    header('Content-Type: application/json');
+    
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
